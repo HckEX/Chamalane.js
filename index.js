1550f122bf4185b5d54151cffeb8042f0178349c
unknown <ltj0623@gmail.com>
2019-07-30 13:53:42 +0900

    Discord BOT : setActivity test done

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const pubgClient = new pubg.Client(PUBG_KEY, 'kakao');
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-  client.user.setActivity('개발', { type : 'PLAYING' });
+  client.user.setActivity('일', { type : 'PLAYING' });
 });
 
 client.on('message', msg => {
