10c6561c126af21de251ce83b8858509a4f39369
unknown <ltj0623@gmail.com>
2019-08-03 22:43:35 +0900

    Bulletin board test #7

--- models/Post.js
+++ models/Post.js
@@ -2,7 +2,7 @@ var mongoose = require("mongoose");
 
 var postSchema = mongoose.Schema({
   title:{type:String, required:true},
-  body:{type:String},
+  body:{type:String, required:true},
   createdAt:{type:Date, default:Date.now},
   updatedAt:{type:Date},
 },{
