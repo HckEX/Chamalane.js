bbbed641f75b5d790f0909784963ec1dcd8420b1
unknown <ltj0623@gmail.com>
2019-08-10 22:15:37 +0900

    login test #1

--- routes/main.js
+++ routes/main.js
@@ -4,8 +4,49 @@ var router = express.Router();
 router.get("/", function(req, res){
   res.render("main/index");
 });
+
 router.get("/helloworld", function(req, res){
   res.render("main/helloworld");
 });
+
+router.get("/login", function (req,res) {
+  var username = req.flash("username")[0];
+  var errors = req.flash("errors")[0] || {};
+  res.render("main/login", {
+   username:username,
+   errors:errors
+  });
+ });
+
+ router.post("/login",
+ function(req,res,next){
+  var errors = {};
+  var isValid = true;
+  if(!req.body.username){
+   isValid = false;
+   errors.username = "Username is required!";
+  }
+  if(!req.body.password){
+   isValid = false;
+   errors.password = "Password is required!";
+  }
+
+  if(isValid){
+   next();
+  } else {
+   req.flash("errors",errors);
+   res.redirect("/login");
+  }
+ },
+ passport.authenticate("local-login", {
+  successRedirect : "/",
+  failureRedirect : "/login"
+ }
+));
+
+router.get("/logout", function(req, res) {
+  req.logout();
+  res.redirect("/");
+ });
 
 module.exports = router;
